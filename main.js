86f060952f3803f2213cf3ce5791b4e4f35a4d59
Teya Urena <teyaleoni@gmail.com>
2018-12-25 22:37:43 -0800

    added turns

--- main.js
+++ main.js
@@ -1,23 +1,50 @@
 // this is to grab the cards in the deck
 const deck = document.querySelector('.deck') 
-
-//function to shuffle
-//need to set variables for the array. what do we want to pass through it? ahh right, the cards. 
-function shuffleDeck() {
-
-}
-
-shuffleDeck()
-
 // we need this because we will be pushing all of the clicked cards into this array to be evaluated.
 // it is best to put into the global scope because we are going to be using it a lot.
 let openCards = []
 
+//this variable is for the turns for the user
+let turns = 10
+
+
+
+//need to set variables for the array. what do we want to pass through it? ahh right, the cards. 
+//cardsToShuffle is the array for all the elements
+//shuffleCards is a variable to hold all the cards that are going to be shuffled
+//created a for loop to add that specific card to the deck
+function shuffleDeck() {
+	const cardsToShuffle = Array.from(document.querySelectorAll('.deck li'))
+	const shuffledCards = shuffle(cardsToShuffle)
+	for (card of shuffledCards) {
+		deck.appendChild(card)
+	}
+}
+
+shuffleDeck()
+
+//function to shuffle
+//you need this because it will define shuffle for the function aboe.
+function shuffle(array) {
+  var shuf = array.length, t, i;
+  while (shuf) {
+    i = Math.floor(Math.random() * shuf--);
+    t = array[shuf];
+    array[shuf] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
+
+
+
 //this is a click handler. an event was put onto deck, which means it listens for every card to be clicked. 
 //conditionals were put on the handler so that the user can only click up to 2 cards at a time
-//the clicks will toggle the card andn push it into the openCards array
+//the clicks will toggle the card and push it into the openCards array
 //check: if they do click 2 cards, console.log 'dos cards'
-//added .includes b/c we only want the click to be displayed and added to the openCard array if it's not already in there.
+//added (.includes) b/c we only want the click to be displayed and added to the openCard array if it's not already in there.
 //this is because we only want unique card clicks
 deck.addEventListener('click', event => {
 	const clickTarget = event.target
@@ -49,6 +76,21 @@ function addOpenCard (clickTarget) {
 	openCards.push(clickTarget)
 }
 
+//this function is for the user turns
+//for each move it adds a point 
+//we use querySelector to target the turns
+// function addTurns() {
+// 	moves++
+// 	const turnsText = document.querySelector('.turns')
+// 	turnsText.innerHTML = turns
+// }
+
+//we want the moves to add when BOTH CARDS are displayed. 
+//so we need to add a length conditional 
+if (openCards.length === 2) {
+	checkForMatch(clickTarget)
+	addTurns
+}
 
 
 
@@ -58,7 +100,7 @@ function addOpenCard (clickTarget) {
 //if the [0] is equal to [1] then..
 // we want to toggle the match on both elements and then reset the array
 //if they dont match we want to reset the array and turn the toggle off
-//time out is a callback function to give the look of a "turn" letes the user see both of the cards (1000ms) before turning over
+//time out is a callback function to give the look of a "turn" lets the user see both of the cards (500ms) before turning over
 //WHERE YOU PUT OPENCARDS MATTERS. ORDER MATTERS HERE.
 function checkForMatch () {
 	if (
@@ -68,15 +110,23 @@ function checkForMatch () {
 		openCards[0].classList.toggle('match')
 		openCards[1].classList.toggle('match')
 		openCards = []
+
 	} else {
 		setTimeout(() => {
 			openCard(openCards[0])
 			openCard(openCards[1])
+			turns--
 			openCards = []
+			
 		}, 500)
 		
 	}
+	$('.turns').html(`Turns Left: ${turns}`)
+
 }
+
+
+
 
 
 
